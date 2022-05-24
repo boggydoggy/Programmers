092e6ada08aa946811554697c13b9d3a0c4b4715
boggydoggy <zhd401@naver.com>
2022-05-24 18:48:29 +0900

    chore: change Best Rank And Worst Rank Problem, delete 1st condition

--- BestRankAndWorstRank/src/Solution.java
+++ BestRankAndWorstRank/src/Solution.java
@@ -10,30 +10,27 @@ class Solution {
         for(int i = 1; i <= 6; i++) {
             rules.put(matchedNumbers--, i);
         }
-        rules.put(matchedNumbers, 6);
+        rules.put(matchedNumbers, 6); //1등부터 6등까지의 조건등록
+
         for(int i : win_nums) {
             for(int j : lottos) {
                 if(i == j) {
-                    confirmCount++;
+                    confirmCount++; //같은 수가 있으면 카운트 상승
                 }
             }
         }
         for(int i : lottos) {
             if(i == 0) {
-                zeroCount++;
+                zeroCount++; //0의 갯수 확인
             }
         }
 
         int[] answer = new int[2];
 
-        if(confirmCount == 6) {
-            answer[0] = 1;
-            answer[1] = 1;
-        }
-        else {
-            answer[0] = rules.get(confirmCount + zeroCount);
-            answer[1] = rules.get(confirmCount);
-        }
+
+        answer[0] = rules.get(confirmCount + zeroCount); //맞은 수와 빈 자리의 수 모두 맞을 경우가 최고 등수
+        answer[1] = rules.get(confirmCount); //맞은 수만 최종 결과일 경우가 최저 등수
+
 
         return answer;
     }
